ea7749f1bde31828988d79460ab4ea035d999232
Boris MBARGA <bmbarga@e2r10p3.42.fr>
2016-10-22 17:38:53 +0200

    IOperand interface implemented

---
+++ includes/IOperand.class.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IOperand.class.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmbarga <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/22 17:37:10 by bmbarga           #+#    #+#             */
+/*   Updated: 2016/10/22 17:37:53 by bmbarga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IOPERAND_CLASS_HPP
+# define IOPERAND_CLASS_HPP
+
+class IOperand
+{
+	public:
+		virtual int getPrecision( void ) const = 0; // Precision of the type of the instance
+		virtual eOperandType getType( void ) const = 0; // Type of the instance
+		virtual IOperand const * operator+( IOperand const & rhs ) const = 0; // Sum
+		virtual IOperand const * operator-( IOperand const & rhs ) const = 0; // Difference
+		virtual IOperand const * operator*( IOperand const & rhs ) const = 0; // Product
+		virtual IOperand const * operator/( IOperand const & rhs ) const = 0; // Quotient
+		virtual IOperand const * operator%( IOperand const & rhs ) const = 0; // Modulo
+		virtual std::string const & toString( void ) const = 0; // String representation of the instance
+		virtual ~IOperand( void ) {}
+};
+
+#endif 
