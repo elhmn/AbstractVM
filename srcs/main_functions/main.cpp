427c3748b30d9452d2aba9f5f534b5a41f81d211
Boris MBARGA <bmbarga@e2r10p3.42.fr>
2016-10-23 13:50:17 +0200

    int\v_t test

--- srcs/main_functions/main.cpp
+++ srcs/main_functions/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bmbarga <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/22 16:52:42 by bmbarga           #+#    #+#             */
-/*   Updated: 2016/10/22 17:49:10 by bmbarga          ###   ########.fr       */
+/*   Updated: 2016/10/23 13:49:52 by bmbarga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 */
 
 #include <iostream>
+#include <cstdint>
 
 int		main(int ac, char **av)
 {
@@ -30,9 +31,24 @@ int		main(int ac, char **av)
 		std::cout << "I read from standard input" << std::endl; //Debug
 	else
 	{
-		//Read data from file.avm
-		while (42)
-			std::cout << "je suis con" << std::endl;
+		int8_t	i_8;
+		int16_t	i_16;
+		int32_t	i_32;
+
+		i_8 = static_cast<int8_t>(8000); //Debug
+		i_16 = static_cast<int16_t>(8000); //Debug
+		i_32 = static_cast<int32_t>(8000); //Debug
+		std::cout << "i_8 = [" << i_8 << "]" << std::endl; //Debug
+		std::cout << "int8_max = [" << INT8_MAX << "]" << std::endl; //Debug
+		std::cout << "int8_max = [" << INT8_MIN << "]" << std::endl; //Debug
+		
+		std::cout << "i_16 = [" << i_16 << "]" << std::endl; //Debug
+		std::cout << "int16_max = [" << INT16_MAX << "]" << std::endl; //Debug
+		std::cout << "int16_max = [" << INT16_MIN << "]" << std::endl; //Debug
+		
+		std::cout << "i_32 = [" << i_32 << "]" << std::endl; //Debug
+		std::cout << "int32_max = [" << INT32_MAX << "]" << std::endl; //Debug
+		std::cout << "int32_max = [" << INT32_MIN << "]" << std::endl; //Debug
 	}
 	return (0);
 }
